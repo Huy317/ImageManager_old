ba92afd02850f1d4a4b2d36f06b853f7179571fe
Doktah_TarrotCat <makotohiname@gmail.com>
2024-06-16 17:32:53 +0700

    initial load in all image from folder

---
+++ src/NetBeansProjects/ImageManager/src/main/java/imagemanager/ImageManager.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package imagemanager;
+
+import Frame.MainFrame;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class ImageManager {
+
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+        frame.setVisible(true);
+    }
+}
